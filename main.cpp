c224bc5a6b7457833a0e92866ae669b296b3b885
Vadim <rvdagi05@yandex.ru>
2022-10-09 11:17:48 +0300

    Result

--- main.cpp
+++ main.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 
-void encrypt_caesar(std::string text, int num) {
+//получение зашифрованного символа
+char getSymbol(char sym, char startSym, int step, int sizeABC) {
+    int tmp = (sym - startSym + step) % sizeABC + startSym;
+    bool pos = tmp < startSym;
+    return pos ? tmp + sizeABC : tmp;
+}
+
+//шифрование
+//получает текст для зашифровки text, шаг step, размер алфавита sizeABC
+std::string encrypt_caesar(std::string text, int step, int sizeABC) {
     std::string encryptText;
 
     for (int i = 0; i < text.length(); i++) {
-        if (text[i] >= 'A' && text[i] <= 'Z') {
-
+        if (text[i] == 32) {
+            encryptText += " ";
+        } else if (text[i] >= 'A' && text[i] <= 'Z') {
+            encryptText += getSymbol(text[i], 'A', step, sizeABC);
+        } else if (text[i] >= 'a' && text[i] <= 'z') {
+            encryptText += getSymbol(text[i], 'a', step, sizeABC);
+        } else {
+            encryptText += text[i];
         }
-        encryptText += text[i] + 10;
     }
-
-    std::cout << "1encryptText: " << encryptText << std::endl;
-    std::cout << "2encryptText: " << ('R' - 'A' + 10) % 26 + 'A' << std::endl;
-    std::cout << (char) 66;
+    return encryptText;
 }
 
-void decrypt_caesar(std::string text, int num) {
-
+//засшифровка
+std::string decrypt_caesar(std::string text, int step, int sizeABC) {
+    return encrypt_caesar(text, -step, sizeABC);
 }
 
 int main() {
-    std::string text = "abracadabra";
+    //исходный текст
+    std::string text = "The quick brown fox jumps over the lazy dog";
+    int step = 0;
+    //размер алфавита
+    int sizeABC = 26;
 
-
-    int num = 10;
     std::cout << "Enter text and num: " << std::endl;
-//    std::getline(std::cin, text);
-//    std::cin >> num;
-    encrypt_caesar(text, num);
+    std::getline(std::cin, text);
+    std::cin >> step;
 
-//    std::cout << text << std::endl;
+    std::cout << "encrypt text: " << encrypt_caesar(text, step, sizeABC) << std::endl;
+    std::cout << "decrypt text: " << decrypt_caesar(text, step, sizeABC) << std::endl;
+
     return 0;
 }
